8146f144556fb1dd15f99e7156adbfff10a37418
Danny Feng (Yanming) <u6611178@anu.edu.au>
2018-03-21 17:55:46 +1100

    finished player class

--- src/comp1110/ass2/Player.java
+++ src/comp1110/ass2/Player.java
@@ -1,4 +1,23 @@
 package comp1110.ass2;
 
+import java.util.Scanner;
+
 public class Player {
+    String playerName;
+    int playerID;
+
+    public Player(int playerID, String playerName) {
+        this.playerName = playerName;
+        this.playerID = playerID;
+    }
+
+    public String getName() {
+        return playerName;
+    }
+
+    @Override
+    public String toString(){
+        return playerID + "" + playerName;
+    }
+
 }
