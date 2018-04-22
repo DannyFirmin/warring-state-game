8780cbc97d97e3dbe123c9fbd65e2e3a5a4e683b
u6611178 <u6611178@anu.edu.au>
2018-04-22 17:59:32 +1000

    wrote Junit test for recode and decode

---
+++ tests/comp1110/ass2/DeCodeTest.java
@@ -0,0 +1,34 @@
+package comp1110.ass2;
+
+import org.junit.Test;
+import static comp1110.ass2.WarringStatesGame.decode;
+import static org.testng.Assert.assertEquals;
+
+public class DeCodeTest {
+    @Test
+    public void testLetters(){
+        int A = 0;
+        char a = decode(A);
+        assertEquals(a,'A');
+
+        int Z = 25;
+        char z = decode(Z);
+        assertEquals(z,'Z');
+
+        int D = 3;
+        char d = decode(D);
+        assertEquals(d,'D');
+    }
+
+    @Test
+    public void testNumbers(){
+        int one = 27;
+        char x = decode(one);
+        assertEquals(x, '1');
+
+        int nine = 35;
+        char y = decode(nine);
+        assertEquals(y,'9');
+
+    }
+}
