4c26244bb2e57b4870c6ae99000f4fbe1bc11b47
Danny Feng (Yanming) <u6611178@anu.edu.au>
2018-05-04 23:27:20 +1000

    JavaFX code merge

--- src/comp1110/ass2/gui/Game.java
+++ src/comp1110/ass2/gui/Game.java
@@ -1,6 +1,9 @@
 package comp1110.ass2.gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Game extends Application {
@@ -15,6 +18,10 @@ public class Game extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("wsg.fxml"));
+        primaryStage.setTitle("Warring States Game (Preview)");
+        primaryStage.setScene(new Scene(root, BOARD_WIDTH, BOARD_HEIGHT));
+        primaryStage.show();
 
     }
 }
